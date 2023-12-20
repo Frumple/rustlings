c18691183d0d42f79353afa891df5df3a11a2b9a
Frumple <frumple1@gmail.com>
2023-12-20 01:07:46 +0000

    Pending changes exported from your codespace

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
@@ -39,8 +37,25 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        add_goals(&mut scores, team_1_name, team_1_score, team_2_score);
+        add_goals(&mut scores, team_2_name, team_2_score, team_1_score);
+
+        // let team_1_entry = scores.entry(team_1_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+        // team_1_entry.goals_scored += team_1_score;
+        // team_1_entry.goals_conceded += team_2_score;
+
+        // let team_2_entry = scores.entry(team_2_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+        // team_2_entry.goals_scored += team_2_score;
+        // team_2_entry.goals_conceded += team_1_score;
     }
     scores
+}
+
+fn add_goals(scores: &mut HashMap<String, Team>, team_name: String, goals_scored: u8, goals_conceded: u8) {
+    let entry = scores.entry(team_name).or_insert(Team { goals_scored: 0, goals_conceded: 0 });
+    entry.goals_scored += goals_scored;
+    entry.goals_conceded += goals_conceded;
 }
 
 #[cfg(test)]
